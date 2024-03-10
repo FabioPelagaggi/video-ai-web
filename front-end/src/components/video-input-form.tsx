57507116fcbb250836fe88feb17cb5ab39e61882
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2024-03-10 18:54:03 -0300

    Video Preview, React states

--- front-end/src/components/video-input-form.tsx
+++ front-end/src/components/video-input-form.tsx
@@ -3,25 +3,72 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { useMemo, useRef, useState } from "react";
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const promptInput = useRef<HTMLTextAreaElement>(null);
+
+  function handleFileSelected(event: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget;
+
+    if (!files) return;
+
+    const selectedFile = files[0];
+
+    setVideoFile(selectedFile);
+  }
+
+  function handleVideoUpload(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const prompt = promptInput.current?.value;
+
+    if (!videoFile) return;
+
+
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) return null;
+
+    return URL.createObjectURL(videoFile);
+  }, [videoFile]);
+
   return (
-    <form className="space-y-6">
+    <form onSubmit={handleVideoUpload} className="space-y-6">
       <label
         htmlFor="video"
-        className="text-primary border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center hover:bg-primary-foreground"
+        className="relative text-primary border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center hover:bg-primary-foreground"
       >
-        <FileVideo className="w-8 h-8" />
-        Upload Video
+        {previewURL ? (
+          <video
+            src={previewURL}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
+        ) : (
+          <>
+            <FileVideo className="w-8 h-8" />
+            Upload Video
+          </>
+        )}
       </label>
 
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
 
       <Separator />
 
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Add Prompts...</Label>
         <Textarea
+          ref={promptInput}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Add video keywords separated by commas (,)."
